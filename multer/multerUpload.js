356712c473125d69fe9ff206bb90a16b6a6ca960
saidFit <saidballoaze123032@gmail.com>
2023-04-12 23:59:28 +0000

    u

--- multer/multerUpload.js
+++ multer/multerUpload.js
@@ -31,12 +31,11 @@ const multer = require('multer');
 
 
 const imageStorage = multer.diskStorage({
-        destination: "uploads",
-        filename: (req, file, cb) => {
-          cb(null, file.fieldname + '_' + Date.now() 
-             + path.extname(file.originalname))
-            // file.fieldname is name of the field (image)
-            // path.extname get the uploaded file extension
+      destination:function (req,file,cb) {
+        cb(null,"uploads")
+    },
+    filename:function (req,file,cb) {
+        cb(null,file.originalname)
     }
 });
 
