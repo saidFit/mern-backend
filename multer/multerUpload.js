b19be226d0e758be2077338d6d611e15fd883a97
saidFit <saidballoaze123032@gmail.com>
2023-04-30 22:55:54 +0100

    mod1

--- multer/multerUpload.js
+++ multer/multerUpload.js
@@ -9,24 +9,24 @@ const multer = require('multer');
 //     }
 // });
 
-const storage = multer.diskStorage({
-    destination:function (req,file,cb) {
-        cb(null,"uploads")
-    },
-    filename:function (req,file,cb) {
-        cb(null,file.originalname)
-    }
-})
-// // // const filefilter = (req, file, cb) => {
-// // //     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' 
-// // //         || file.mimetype === 'image/jpeg' || file.mimetype === 'image/jfif'){
-// // //             cb(null, true);
-// // //         }else {
-// // //             cb(null, false);
-// // //         }
-// // // }
+// const storage = multer.diskStorage({
+//     destination:function (req,file,cb) {
+//         cb(null,"uploads")
+//     },
+//     filename:function (req,file,cb) {
+//         cb(null,file.originalname)
+//     }
+// })
+// // // // const filefilter = (req, file, cb) => {
+// // // //     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' 
+// // // //         || file.mimetype === 'image/jpeg' || file.mimetype === 'image/jfif'){
+// // // //             cb(null, true);
+// // // //         }else {
+// // // //             cb(null, false);
+// // // //         }
+// // // // }
 
-const upload = multer({storage: storage});
+// const upload = multer({storage: storage});
 
 
 
@@ -66,6 +66,24 @@ const upload = multer({storage: storage});
 // })
 // var upload = multer({ storage: storage })
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null,file.originalname)
+  },
+});
 
+// const storage = multer.diskStorage({
+//     destination:function (req,file,cb) {
+//         cb(null,"uploads")
+//     },
+//     filename:function (req,file,cb) {
+//         cb(null,file.originalname)
+//     }
+// })
+
+const upload = multer({ storage: storage });
 
 module.exports = {upload};
--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/Message',RouterMessage)
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // ----------middlewere--------//
 
