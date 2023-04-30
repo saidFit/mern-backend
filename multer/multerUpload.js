43423bd39690185351e1c3707b645c378183370b
saidFit <saidballoaze123032@gmail.com>
2023-04-30 17:09:59 +0100

    mod1

--- multer/multerUpload.js
+++ multer/multerUpload.js
@@ -70,24 +70,22 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    const extension = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + Date.now() + extension);
-  }
-});
-
-const upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-      const extension = path.extname(file.originalname);
-      if (extension !== '.jfif' && extension !== '.jpeg' && extension !== '.jpg' && extension !== '.png') {
-        cb(new Error('File type not allowed'));
-      }
-      cb(null, true)
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
     }
-  });
+});
+const filefilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' 
+        || file.mimetype === 'image/jpeg'){
+            cb(null, true);
+        }else {
+            cb(null, false);
+        }
+}
+
+const upload = multer({storage: storage, fileFilter: filefilter});
 
 module.exports = {upload};
