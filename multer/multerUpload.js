68ad73fee515bc2b21255c799830e54f8489af67
saidFit <saidballoaze123032@gmail.com>
2023-04-30 19:57:53 +0100

    mod1

--- multer/multerUpload.js
+++ multer/multerUpload.js
@@ -67,25 +67,37 @@ const multer = require('multer');
 // var upload = multer({ storage: storage })
 
 // Set up multer storage and file filter
-const path = require('path');
-
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
-    }
+  destination: './uploads/',
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
 });
-const filefilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' 
-        || file.mimetype === 'image/jpeg'){
-            cb(null, true);
-        }else {
-            cb(null, false);
-        }
+
+// Initialize Multer
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }, // Set file size limit to 1 MB
+  fileFilter: function(req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single('profileImage'); // Set name of file field in HTML form
+
+// Check file type
+function checkFileType(file, cb) {
+  // Allowed file extensions
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check extension
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check MIME type
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images only!');
+  }
 }
 
-const upload = multer({storage: storage, fileFilter: filefilter});
 
 module.exports = {upload};
